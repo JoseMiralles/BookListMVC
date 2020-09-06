ea6b6a023678c4f92733ef4930632096651dc055
Jose <josemiralles1991@gmail.com>
2020-09-06 03:37:20 -0400

    Initial commit

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+
+        // Can be queried with LINQ.
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
